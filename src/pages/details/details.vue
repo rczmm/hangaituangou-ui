09a1475faf2dba93488dba4d26389070eb67ed08
rcz <3179623450@qq.com>
2025-01-18 16:36:25 +0800

    style(details): 调整评论卡片宽度
    - 在 card-comment 类的样式中添加宽度属性，设置为 80%
    - 此修改使评论卡片的宽度更加合理，避免内容显示不正确

--- src/pages/details/details.vue
+++ src/pages/details/details.vue
@@ -15,10 +15,7 @@
     <!-- 列表   -->
     <!-- 带有追评的评论   -->
 
-    <view class="card-comment"
-          style="margin-top: 100px; border-radius: 25px;background-color: red ;width: 80%;"
-
-    >
+    <view class="card-comment">
       <nut-comment
         imagesRows="multi"
         :images="cmt.images"
@@ -27,7 +24,6 @@
         :follow="cmt.follow"
         @clickImages="clickImages"
       ></nut-comment>
-
     </view>
 
 
@@ -36,15 +32,16 @@
 
 
     <!-- 商品规格选择   -->
-    <!--    <nut-cell :title="`基础用法`" desc="" @click="base = true"></nut-cell>-->
-    <!--    <nut-sku-->
-    <!--      v-model:visible="base"-->
-    <!--      :sku="data.sku"-->
-    <!--      :goods="data.goods"-->
-    <!--      @selectSku="selectSku"-->
-    <!--      @clickBtnOperate="clickBtnOperate"-->
-    <!--      @close="close"-->
-    <!--    ></nut-sku>-->
+    <nut-cell :title="`基础用法`" desc="" @click="base = true"></nut-cell>
+    <nut-sku
+      style="background-color: red"
+      v-model:visible="base"
+      :sku="data.sku"
+      :goods="data.goods"
+      @selectSku="selectSku"
+      @clickBtnOperate="clickBtnOperate"
+      @close="close"
+    ></nut-sku>
 
   </view>
 </template>
@@ -52,6 +49,69 @@
 <script setup lang="ts">
 
 import {ref} from "vue";
+
+import './details.scss'
+
+
+const requestData = {
+  "Sku": [{
+    "id": 1,
+    "name": "颜色",
+    "list": [{"name": "亮黑色", "id": 100016015112, "active": true, "disable": false}, {
+      "name": "釉白色",
+      "id": 100016015142,
+      "active": false,
+      "disable": false
+    }, {"name": "秘银色", "id": 100016015078, "active": false, "disable": false}, {
+      "name": "夏日胡杨",
+      "id": 100009064831,
+      "active": false,
+      "disable": false
+    }, {"name": "秋日胡杨", "id": 100009064830, "active": false, "disable": false}]
+  }, {
+    "id": 2,
+    "name": "版本",
+    "list": [{"name": "8GB+128GB", "id": 100016015102, "active": true, "disable": false}, {
+      "name": "8GB+256GB",
+      "id": 100016015122,
+      "active": false,
+      "disable": false
+    }]
+  }, {
+    "id": 3,
+    "name": "版本",
+    "list": [{"name": "4G（有充版）", "id": 100016015103, "active": true, "disable": false}, {
+      "name": "5G（有充版）",
+      "id": 100016015123,
+      "active": false,
+      "disable": false
+    }, {"name": "5G（无充版）", "id": 100016015104, "active": true, "disable": true}, {
+      "name": "5G（无充）质保换新版",
+      "id": 100016015125,
+      "active": false,
+      "disable": false
+    }]
+  }],
+  "Goods": {
+    "skuId": "100016015112",
+    "price": "4599.00",
+    "imagePath": "//m.360buyimg.com/mobilecms/s750x750_jfs/t1/210630/17/8651/208682/618a5bd6Eddc8ea0e/b5e55e1a03bc0126.jpg!q80.dpg.webp"
+  },
+  "imagePathMap": {
+    "100016015112": "//m.360buyimg.com/mobilecms/s750x750_jfs/t1/210630/17/8651/208682/618a5bd6Eddc8ea0e/b5e55e1a03bc0126.jpg!q80.dpg.web",
+    "100016015142": "//img14.360buyimg.com/n4/jfs/t1/216079/14/3895/201095/618a5c0cEe0b9e2ba/cf5b98fb6128a09e.jpg",
+    "100016015078": "//img14.360buyimg.com/n4/jfs/t1/215845/12/3788/221990/618a5c4dEc71cb4c7/7bd6eb8d17830991.jpg",
+    "100009064831": "//img14.360buyimg.com/n4/jfs/t1/203247/8/14659/237368/618a5c87Ecc968774/b0bb25331e5e2d1a.jpg",
+    "100009064830": "//img14.360buyimg.com/n4/jfs/t1/160950/40/25098/234168/618a5cb9E65ba975e/7f8f93ea7767a51b.jpg"
+  }
+}
+
+
+const data = ref({
+  sku: requestData.Sku,
+  goods: requestData.Goods
+})
+
 
 const clickImages = (imgs) => {
   console.log('进行图片展示', imgs)
@@ -124,6 +184,27 @@ const swiperList = ref([
   'https://storage.360buyimg.com/jdc-article/welcomenutui.jpg',
   'https://storage.360buyimg.com/jdc-article/fristfabu.jpg'
 ])
+
+// 切换规格类目
+const selectSku = (ss) => {
+  const {sku, skuIndex, parentSku, parentIndex} = ss
+  if (sku.disable) return false
+  requestData.sku[parentIndex].list.forEach((s) => {
+    s.active = s.id == sku.id
+  })
+  requestData.goods = {
+    skuId: sku.id,
+    price: '4599.00',
+    imagePath: '//img14.360buyimg.com/n4/jfs/t1/215845/12/3788/221990/618a5c4dEc71cb4c7/7bd6eb8d17830991.jpg'
+  }
+}
+// 底部操作按钮触发
+const clickBtnOperate = (op) => {
+  console.log('点击了操作按钮', op)
+}
+// 关闭商品规格弹框
+const close = () => {
+}
 
 </script>
 <style scoped>
